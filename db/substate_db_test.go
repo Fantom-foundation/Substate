a7d93271a19612d88ca566a06b512a79f793485f
Shevchuk <evgen.sheff@gmail.com>
2024-04-02 16:25:02 +0700

    fix

--- db/substate_db_test.go
+++ db/substate_db_test.go
@@ -120,8 +120,6 @@ func createDbAndPutSubstate(dbPath string) (*substateDB, error) {
 	h2 := types.Hash{}
 	h2.SetBytes(nil)
 
-	tt := &testSubstate
-	_ = tt
 	testSubstate.InputSubstate[types.Address{1}] = substate.NewAccount(1, new(big.Int).SetUint64(1), h1[:])
 	testSubstate.OutputSubstate[types.Address{2}] = substate.NewAccount(2, new(big.Int).SetUint64(2), h2[:])
 	testSubstate.Env.BlockHashes[1] = types.BytesToHash([]byte{1})
