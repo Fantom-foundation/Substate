2f34ea61a20b1757f237cbaffb7a6e7f52a23533
Rapol <rapol@fantom.foundation>
2024-09-10 20:26:09 +0200

    bugfix2

--- substate/world_state.go
+++ substate/world_state.go
@@ -3,7 +3,6 @@ package substate
 import (
 	"bytes"
 	"fmt"
-	"math/big"
 	"strings"
 
 	"github.com/holiman/uint256"
@@ -38,7 +37,7 @@ func (ws WorldState) Merge(y WorldState) {
 
 			// overwrite yAcc details in ws by y
 			ws[yAddr].Nonce = yAcc.Nonce
-			ws[yAddr].Balance = types.BytesToUint256(yAcc.Balance)
+			ws[yAddr].Balance = yAcc.Balance
 			ws[yAddr].Code = make([]byte, len(yAcc.Code))
 			copy(ws[yAddr].Code, yAcc.Code)
 		} else {
