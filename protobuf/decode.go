238c27e3c5a55f638c44629229d360c531a8309f
Rapol <rapol@fantom.foundation>
2024-09-12 10:52:20 +0200

    debug gfc, gtp7

--- protobuf/decode.go
+++ protobuf/decode.go
@@ -228,7 +228,7 @@ func (msg *Substate_TxMessage) decode(lookup DbGetCode) (*substate.Message, erro
 
 	// London hard fork, EIP-1559: Fee market
 	var gasFeeCap *big.Int = types.BytesToBigInt(msg.GetGasPrice())
-	switch txTyp := *t.TxType; txType {
+	switch txTyp := *t.TxType; txTyp {
 	case Substate_TxMessage_TXTYPE_DYNAMICFEE:
 	case Substate_TxMessage_TXTYPE_BLOB:
 		fmt.Println("I'm doing this!")
